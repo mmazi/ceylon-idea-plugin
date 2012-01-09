0cc42a6a49434a6e1c98d8ecadc05fd61746ba3c
Andrey Ponomarev <andrey.ponomarev@gmail.com>
2012-01-09 13:04:06 +0200

    Renamed test

--- test/org/ceylon/idea/lang/parser/GrammarTest.java
+++ test/org/ceylon/idea/lang/parser/GrammarTest.java
@@ -9,7 +9,7 @@ import static org.ceylon.idea.lang.lexer.CeylonToken.*;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class ParserRulesTest {
+public class GrammarTest {
     private final MockPsiBuilder builder = new MockPsiBuilder();
 
     @Test
